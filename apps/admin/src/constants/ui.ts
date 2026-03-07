d4ec90bb305c3c30a0eea1522cba2fa27174df82
DumbDaiDai <3234627960@qq.com>
2026-03-07 23:46:55 +0800

    feat: 根据设计意见更改danger color

--- apps/admin/src/constants/ui.ts
+++ apps/admin/src/constants/ui.ts
@@ -12,6 +12,6 @@ export const THEME_VAR_RECORD = {
   primaryColor: "#F58888",
   successColor: "#49DF17",
   warningColor: "#ADDF17",
-  dangerColor: "#EE0E0E",
+  dangerColor: "#E53935",
   infoColor: "#3A87FA"
 } as const;

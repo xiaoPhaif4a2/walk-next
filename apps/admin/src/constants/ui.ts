be33f61f5968a0b72c5097fda7af58341e73e5d8
DumbDaiDai <3234627960@qq.com>
2026-03-07 11:48:15 +0800

    feat: 加深danger主题色颜色

--- apps/admin/src/constants/ui.ts
+++ apps/admin/src/constants/ui.ts
@@ -12,6 +12,6 @@ export const THEME_VAR_RECORD = {
   primaryColor: "#F58888",
   successColor: "#49DF17",
   warningColor: "#ADDF17",
-  dangerColor: "#ED7461",
+  dangerColor: "#EE0E0E",
   infoColor: "#3A87FA"
 } as const;
